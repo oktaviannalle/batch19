aa8c1775577c937ab24ac51174a2e952b722f671
= <=>
2026-06-24 14:35:13 +0700

    Game Othello

--- OthelloGame/Program.cs
+++ OthelloGame/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using OthelloGame.Domain.Enum;      
-using OthelloGame.Domain.Interfaces; 
-using OthelloGame.Domain.Class;      
+using OthelloGame.Domain.Enum;
+using OthelloGame.Domain.Interfaces;
+using OthelloGame.Domain.Class;
+using OthelloGame.Domain;
 
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("===SELAMAT DATANG DI GAME OTHELLO===");
-    }
-}
+Console.Clear();
+Console.WriteLine("=== SELAMAT DATANG DI GAME OTHELLO ===");
+
+IPlayer player1 = new Player("Player 1 (Hitam)", PieceColor.Black);
+IPlayer player2 = new Player("Player 2 (Putih)", PieceColor.White);
+var players = new List<IPlayer> { player1, player2 };
